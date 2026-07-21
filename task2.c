2d2b7c9ab4a81409e3403b80ed9bf993af4834e2
VibhaNayak05 <vibhanayak123@gmail.com>
2026-07-21 10:56:18 +0530

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
-
 const char* get_crease(int n, int x) {
-  
     if (n == 1) {
         return "Valley";
     }
-
-    
-    int mid = 1 << (n - 1); 
-
+    int mid = 1 << (n - 1);
     if (x == mid) {
-     
         return "Valley"; 
     } 
     else if (x < mid) {
@@ -20,10 +13,8 @@ const char* get_crease(int n, int x) {
         return get_crease(n - 1, x);
     } 
     else {
-       
         int mirrored_x = mid - (x - mid);
         const char* left_val = get_crease(n - 1, mirrored_x);
-        
         if (strcmp(left_val, "Valley") == 0) {
             return "Mountain";
         } else {
